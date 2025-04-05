eb4b94fae490bb39fd3ef7c9cccfeb2bea965878
roelandkulk <roelandkulk@gmail.com>
2025-04-05 15:21:48 +0200

    Add new Bean button

--- app/beans/page.tsx
+++ app/beans/page.tsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import {prisma} from "@/prisma/client";
 import {Bean} from "@prisma/client";
-import {Table} from '@radix-ui/themes';
+import {Button, Table} from '@radix-ui/themes';
 import DeleteBeanIcon from "@/app/beans/DeleteBeanIcon";
+import Link from "next/link";
 
 const EspressoPage = async () => {
 
     const bean: Bean[] = await prisma.bean.findMany()
 
     return (
-        <Table.Root variant='surface'>
-            <Table.Header>
-                <Table.Row>
-                    <Table.ColumnHeaderCell>Bean</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>Roaster</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>Roast level</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-                </Table.Row>
-            </Table.Header>
-
-            <Table.Body>
-                {bean.map( bean =>
-                    <Table.Row key={bean.id}>
-                        <Table.Cell>{bean.name}</Table.Cell>
-                        <Table.Cell>{bean.roaster}</Table.Cell>
-                        <Table.Cell>{bean.roastLevel}</Table.Cell>
-                        <Table.Cell>
-                            <DeleteBeanIcon id={bean.id}/>
-                        </Table.Cell>
+        <>
+            <Button mb="3">
+                <Link href="/beans/new">New Bean</Link>
+            </Button>
+            <Table.Root variant='surface'>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Bean</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>Roaster</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>Roast level</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
                     </Table.Row>
-                )}
-            </Table.Body>
-        </Table.Root>
+                </Table.Header>
+
+                <Table.Body>
+                    {bean.map(bean =>
+                        <Table.Row key={bean.id}>
+                            <Table.Cell>{bean.name}</Table.Cell>
+                            <Table.Cell>{bean.roaster}</Table.Cell>
+                            <Table.Cell>{bean.roastLevel}</Table.Cell>
+                            <Table.Cell>
+                                <DeleteBeanIcon id={bean.id}/>
+                            </Table.Cell>
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table.Root>
+        </>
     )
 }
 export default EspressoPage
