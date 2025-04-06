96b06cddd2cf0fc2cf020cc1d50070406f5957da
roelandkulk <roelandkulk@gmail.com>
2025-04-06 17:27:00 +0200

    New Espresso button

--- app/espressos/page.tsx
+++ app/espressos/page.tsx
@@ -2,7 +2,8 @@ import {prisma} from "@/prisma/client";
 import React from 'react'
 import EspressoSummaryCard from "@/app/espressos/EspressoSummaryCard";
 import {Bean, Espresso,} from "@prisma/client";
-import {Flex} from "@radix-ui/themes";
+import {Button, Flex} from "@radix-ui/themes";
+import Link from "next/link";
 
 
 export type EspressoWithBean = Espresso & {
@@ -21,11 +22,16 @@ const EspressosPage = async () => {
     })
 
     return (
-        <Flex gap="2" wrap="wrap">
-            {espressos.map(espresso =>
-                <EspressoSummaryCard key={espresso.id} espresso={espresso}/>
-            )}
-        </Flex>
+        <>
+            <Button mb="3">
+                <Link href="/espressos/new">New Espresso</Link>
+            </Button>
+            <Flex gap="2" wrap="wrap">
+                {espressos.map(espresso =>
+                    <EspressoSummaryCard key={espresso.id} espresso={espresso}/>
+                )}
+            </Flex>
+        </>
     )
 }
 export default EspressosPage
